c65e74da1a4e1ff981db19d7a7502509eee80802
yesol_lee <thfl3363@hanmail.net>
2022-09-02 21:40:24 +0900

    layout children 추가

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Layout(props) {
+function Layout( { children } ) {
     return (
         <div>
-            
+            { children }
         </div>
     );
 }
