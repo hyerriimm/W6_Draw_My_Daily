93a9711f995f4d1f2f58de848d073f5529d4293f
hyerriimm <gwj06129@naver.com>
2022-09-04 21:48:08 +0900

    uuid패키지/격자이미지 추가,/Home, Add 페이지 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,44 @@
 import { useNavigate } from 'react-router-dom';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import Layout from '../components/layout/Layout'
-import Header from '../components/header/Header';
 import PostList from '../components/postlist/PostList';
 
 function Home() {
   let navigate = useNavigate();
+  
   return (
     <Layout>
-      <Header />
-      <h1>그림일기 Draw My Daily</h1>
-        <button onClick={() => navigate('/add')}>일기쓰기</button>
+      <AddBtnDiv>
+      <button onClick={() => navigate('/add')}>일기 쓰기</button>
+      </AddBtnDiv>
       <PostList />
     </Layout>
   );
 }
 
 export default Home;
+
+const AddBtnDiv = styled.button`
+  border: transparent;
+  background-color: #fff8f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  padding: 30px 0;
+button {
+padding: 5px 20px;
+background-color: rgb(68, 155, 255);
+color: white;
+font-size: 20px;
+font-weight: bold;
+border: transparent;
+border-radius: 20px;
+width: 150px;
+height: 40px;
+&:hover {
+  box-shadow: 0 80px 0 0  rgba(0,0,0,0.25) inset, 
+              0 -80px 0 0 rgba(0,0,0,0.25) inset;
+}
+}
+`;
