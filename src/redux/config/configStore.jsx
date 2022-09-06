c20b488f20c4fc01bb5f3d391bff6e6c9542c75f
yesol_lee <thfl3363@hanmail.net>
2022-09-06 20:15:13 +0900

    yesol 로그아웃 수정

--- src/redux/config/configStore.jsx
+++ src/redux/config/configStore.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import posts from "../modules/postsSlice";
 import comments from "../modules/commentsSlice";
 import users from "../modules/users";
 
 
 const store = configureStore({
     reducer:{
+        posts,
         comments,
         users
     }, middleware: (getDefaultMiddleware) => getDefaultMiddleware ({serializableCheck:false})
